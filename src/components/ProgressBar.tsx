0740c203c5548db8336cf5b589f6c67ac175a709
JailsonSantos <jailson.ads007@gmail.com>
2023-01-22 10:26:29 -0300

    Feat: Finalizando todos os componetes e atualizando a estilização

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -3,15 +3,16 @@ interface ProgressBarProps {
 }
 
 export function ProgressBar(props: ProgressBarProps) {
-
   return (
-    <div className="h-3 rounded-xl bg-zinc-700 w-full mt-4">
+    <div className='h-3 rounded-xl bg-zinc-700 w-full mt-4'>
       <div
         role="progressbar"
         aria-valuenow={props.progress}
-        style={{ width: `${props.progress}%` }}
-        className="h-3 rounded-xl bg-violet-600"
-        aria-label="Progresso de hábitos completados neste dia"
+        aria-label="Progresso de hábitos completados nesse dia"
+        className="h-3 rounded-xl bg-violet-600 transition-all transition-all"
+        style={{
+          width: `${props.progress}%`
+        }}
       />
     </div>
   )
